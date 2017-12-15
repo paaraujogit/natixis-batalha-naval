c1721a88f7e6fc4c484d34761926ed34099ee193
Alexandre Marques <alexandre.morais.marques@gmail.com>
2017-12-15 16:02:28 +0000

    Verifica limites tabuleiro
    Verifica limites tabuleiro

--- BatalhaNaval/src/controller/PosicaoBarcosController.java
+++ BatalhaNaval/src/controller/PosicaoBarcosController.java
@@ -1,10 +1,19 @@
 package controller;
 
 import model.Barco;
+import view.PosicaoBarcosView;
 
 
 public class PosicaoBarcosController {
+ 
+    public boolean verificaLimites (int coord){
+        if (coord >= 0 && coord <=9){
+            return false;
+        } else return true;                
+    }
     
-    
+    public void verificaPosicao(int linha, int coluna, int tamanho, Barco.Orientacao orientacao){
+        
+    }
     
 }
