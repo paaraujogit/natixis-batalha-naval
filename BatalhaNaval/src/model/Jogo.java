af991bcb56527d37a9bc160b8517868d1376d85a
sargeantea <at2394@gmail.com>
2017-12-15 15:27:54 +0000

    update jogada contoller e correcao do nome do jogador 2

--- BatalhaNaval/src/model/Jogo.java
+++ BatalhaNaval/src/model/Jogo.java
@@ -1,32 +1,40 @@
 package model;
 
+public class Jogo {
 
-public class Jogo {     
     enum Turno {
-        jogador1{
-            @Override
-            public Turno mudarTurno() {
-                return Turno.jogador2;
-            }        
-        },
-        jogador2{
-            @Override
-            public Turno mudarTurno() {
-                return Turno.jogador1;
-            }
-        };
+
+        jogador1 {
+                    @Override
+                    public Turno mudarTurno() {
+                        return Turno.jogador2;
+                    }
+                },
+        jogador2 {
+                    @Override
+                    public Turno mudarTurno() {
+                        return Turno.jogador1;
+                    }
+                };
+
         public abstract Turno mudarTurno();
     }
-    
-    Jogador jogador1;
-    Jogador jogado2;
-    
+
+    private Jogador jogador1;
+    private Jogador jogador2;
+
     Turno turno = Turno.jogador1;
-    
-    
-    public void mudarTurno(){
-       turno = turno.mudarTurno();
+
+    public Jogador getJogador1() {
+        return jogador1;
     }
-    
-   
+
+    public Jogador getJogador2() {
+        return jogador2;
+    }
+
+    public void mudarTurno() {
+        turno = turno.mudarTurno();
+    }
+
 }
